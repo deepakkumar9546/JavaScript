a4f8959d768381c199e303bb66de52af58d41ccb
Deepak Kumar <137646137+deepakkumar9546@users.noreply.github.com>
2024-05-23 12:46:39 +0000

    datatypes

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,30 @@
+"use strict";  // treat all JS code as newer version 
+
+// alert(3+3)  //it will not show pop-up when we will run as we are using node js, not browser
+
+console.log(3
+    + 3)  //code readability should be high
+
+
+let name="deepak"
+let age=18
+let isLoggedIn=false
+
+// number => 2 to power 53
+// bigint => for declaring big integer especially in trading and stock market
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => that means value is not assigned
+// symbol => used for uniqueness
+
+
+// object
+
+console.log(typeof "deepak");  //string
+
+console.log(typeof age);  //number
+
+console.log(typeof null);  // null is a object so object will print on the screen
+
+console.log(typeof undefined);  // undefined is itself of type so undefined will print on screen
