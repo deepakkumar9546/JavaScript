49db61d54338eb9d9305995f1b5741b794ea1290
Deepak Kumar <137646137+deepakkumar9546@users.noreply.github.com>
2024-05-23 11:42:54 +0000

    discussion on variables

--- 01_basics/variable.js
+++ 01_basics/variable.js
@@ -2,13 +2,14 @@ const accountId=14432
 let accountEmail="deepak@gmail.com"
 var accountPassword="12345"
 accountCity="Jaipur"
+let accountState;   /*if we print this then it will show the 'undefined' */
 
-// accountId=2 // not allowed
+// accountId=2 // not allowed as it is declared from const
 
 accountEmail="hello@gmail.com"
 accountPassword="21212121"
 accountCity="Bengaluru"
-let accountState;
+
 
 console.log(accountId);
 
