23f87c393da81a6baa7d6dbb6bbb8bc0954ddf10
Deepak Kumar <137646137+deepakkumar9546@users.noreply.github.com>
2024-06-01 05:47:36 +0000

    conversion of operations

--- 01_basics/conversionOperation.js
+++ 01_basics/conversionOperation.js
@@ -26,5 +26,51 @@ let booleanIsLoggedIn=Boolean(isLoggedIn) // to convert in boolean
 let someNumber=33
 
 let stringNumber=String(someNumber) // to convert in string
-console.log(stringNumber)
-console.log(typeof stringNumber) // to check whether that number really converted in string or not 
+// console.log(stringNumber)
+// console.log(typeof stringNumber) // to check whether that number really converted in string or not 
+
+// *****************************operations*************************************** 
+
+let value=3
+let negValue = -value
+//console.log(negValue)  // -3
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2/3);
+//console.log(2**3);
+//console.log(2%3);
+
+let str1="hello"
+let str2=" deepak"
+
+let str3=str1+str2 // addition/concatenation of string
+// console.log(str3)
+
+// console.log("1" + 2)     //12
+
+// console.log(1 + "2")     //12
+
+// console.log ("1" + 2 + 2) //122  (if string comes first then all the numbers gets converted into string and gets attached one after)
+
+// console.log (1 + 2 + "2") //32  (if string is at the last then firstly all the operations gets performed and at last string is get attached)
+
+//console.log((3+4) * 5 % 3);
+
+// console.log(+true);  //1 (return 1 as it is true)
+
+// console.log(+"");    //0 (empty string so return false)
+
+let num1,num2,num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter); //101
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+
+
