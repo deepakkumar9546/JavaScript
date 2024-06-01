f027551fe5363543fd11bc3e478f522ed10a60a5
Deepak Kumar <137646137+deepakkumar9546@users.noreply.github.com>
2024-06-01 04:56:09 +0000

    conversion of datatypes

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,29 @@
+let score="deepak"
+
+//console.log(typeof score)
+//console.log(typeof(score))
+
+let valueInNumber=Number(score) //converted in number
+
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber)
+
+// "33" => 33 (easily converted into number)
+
+// "33abc" => NaN (Not a Number)
+// true => 1, false => 0
+
+let isLoggedIn = ""
+
+let booleanIsLoggedIn=Boolean(isLoggedIn) // to convert in boolean 
+// console.log(booleanIsLoggedIn)
+
+// 1 => true, 0 => false
+// "" => false
+// "deepak" => true
+
+let someNumber=33
+
+let stringNumber=String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
