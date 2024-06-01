7d370fc0598b316198aea8e2a8cdc21f7569d79e
Deepak Kumar <137646137+deepakkumar9546@users.noreply.github.com>
2024-06-01 03:24:01 +0000

    discussion on variables

--- 01_basics/variable.js
+++ 01_basics/variable.js
@@ -18,7 +18,7 @@ prefer not to use var
 because of issue in block scope and functional scope
 */
 
-console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])  // all the values are in tabular form
 
 
 
